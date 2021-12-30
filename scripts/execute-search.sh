7eba672fd24aeb541eff7d1e20d86d7216e57bb2
Carlos Atencio <catencio@unsa.edu.pe>
2021-12-30 07:01:33 -0500

    Search shows the filenames

--- scripts/execute-search.sh
+++ scripts/execute-search.sh
@@ -2,8 +2,9 @@
 
 #paths and files
 stopwords="../data/stopwords/"
-wordlist="generated/words-file.txt"
+wordlist="generated/words-file-treated.txt"
 index="generated/wiki.dat"
+filenameSize="7"
 
 #executing 
-../build/src/search/Search  STOPWORDS=$stopwords WORDLIST=$wordlist INDEXFILE=$index
+../build/src/search/Search  STOPWORDS=$stopwords WORDLIST=$wordlist INDEXFILE=$index FILENAMESZ=$filenameSize
